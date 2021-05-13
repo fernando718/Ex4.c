2c7ca21cb3d6b9b68abe0c10766ff4f39548ee78
fernando718 <82851141+fernando718@users.noreply.github.com>
2021-05-12 21:14:03 -0500

    Create Ex4.c

---
+++ Ex4.c
@@ -0,0 +1,15 @@
+//VALDIVIA GONZALEZ JOSE FERNANDO// //4IV17//
+#include >stdio.h> 
+int main ()
+[
+   float peso, altura, IMC;
+   printf ("\n\n Programa 4");
+   printf ("\n\n calcular el indice de masa corporal");
+   printf ("\n\n dame el peso");
+   scanf ("%f",&peso); 
+   printf ("\n\n dame la altura");
+   scanf ("%f",&altura);
+   IMC= peso/ (altura*altura);
+   printf  ("\n\n el resultado de IMC es el peso entre altura por altura es &f");
+   return 0;
+]
